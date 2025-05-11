bc37a225ccc0da90be8a78b5dea657b5710b81c1
Priyadarshani Sahu <priyadarshanisahu09@gmail.com>
2025-05-11 16:48:07 +0530

    To check score according to character

---
+++ Character.py
@@ -0,0 +1,14 @@
+//Shows the Grade accoring to character
+x=input("Enter a character(O,A,B,C,F):")
+if x=='O':
+	print("Outstanding")
+elif x =='A':
+	print("Very good")
+elif x =='B':
+	print("Good")
+elif x =='C':
+	print("Average")
+elif x =='F':
+	print("Fail") 
+else:
+	print("Input a valid character.")
